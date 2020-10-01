e05f5c247dfbf99f1245d8a517856f9fd433d4e4
Rutuja-Pansare <rutujapansare.22@gmail.com>
2020-10-01 10:41:11 +0530

    Pulled from dev

--- OnlineShoppingAngular/src/app/models/cart.ts
+++ OnlineShoppingAngular/src/app/models/cart.ts
@@ -3,5 +3,7 @@ import { Product } from './product';
 export class Cart
 {
     productModel:Product;
+    userId:number;
+    productId:number;
     quantity:number;
 }
---
+++ OnlineShoppingAngular/src/app/services/CartService.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class CartService
+{
+    constructor(private httpClient:HttpClient)
+    {
+
+    }
+    
+
+
+
+
+}
